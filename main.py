a051673e0a35f764f65340e2f29d3fab6ab9b8fb
Etienne Roulet <32195170+etiiiR@users.noreply.github.com>
2021-11-25 12:16:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pyArango.connection import *
-
+    
 
 if __name__ == "__main__":
   conn = Connection(arangoURL="https://arangodb.roulet.dev", username="root", password="9icKdiH@toH933Cdf*7ixdJADM&")
   db = conn["GDB"]
-  aql = "FOR x IN airports RETURN x._key"
+  aql = "FOR x IN Airports_LE5 RETURN x.Name"
   queryResult = db.AQLQuery(aql, rawResults=True, batchSize=100)
   for key in queryResult:
     print(key)
